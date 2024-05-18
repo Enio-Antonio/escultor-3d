728a4b8b48acc5d9a83c1d30ab43ca035be496d4
Enio-Antonio <enioaclizieri@gmail.com>
2024-05-18 12:12:01 -0300

    Adição da documentação.
    Documentação feita com Doxygen.

---
+++ documentacao-escultor3d/search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sculptor_0',['Sculptor',['../class_sculptor.html',1,'Sculptor'],['../class_sculptor.html#ae8db317044947a64e227530f6553afff',1,'Sculptor::Sculptor(int nx, int ny, int nz)']]],
+  ['setcolor_1',['setColor',['../class_sculptor.html#a4351c930a07887e96d4fdbf251a7867e',1,'Sculptor']]]
+];
---
+++ documentacao-escultor3d/search/functions_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sculptor_0',['Sculptor',['../class_sculptor.html#ae8db317044947a64e227530f6553afff',1,'Sculptor']]],
+  ['setcolor_1',['setColor',['../class_sculptor.html#a4351c930a07887e96d4fdbf251a7867e',1,'Sculptor']]]
+];
